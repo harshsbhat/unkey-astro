b1aa30642b005cfcd73a834e07ec244dff5a49f3
Harsh Shrikant Bhat <90265455+harshsbhat@users.noreply.github.com>
2024-10-17 23:28:55 +0530

    else block removed

--- src/pages/api/ratelimited.ts
+++ src/pages/api/ratelimited.ts
@@ -2,7 +2,7 @@ import type { APIRoute } from 'astro';
 import { unkey } from "../../lib/unkey";
 
 export const GET: APIRoute = async ({ request }) => {
-  
+
   // This is a random UserId Completely fake. If you don't have a UserId you can use any unique identifier here.
   const userId = "userId_BYDjLrSNxkoGI33gfDDLc+ZYwTUGGfsVFEkCQ"
   const ratelimit = await unkey.limit(userId);
@@ -10,7 +10,7 @@ export const GET: APIRoute = async ({ request }) => {
   if (!ratelimit.success) {
     return new Response("Try again later", { status: 429 });
   }
-  if (ratelimit.success) {
+  else {
     return new Response(
       JSON.stringify({
         message: "Ratelimited route here",
@@ -24,8 +24,5 @@ export const GET: APIRoute = async ({ request }) => {
       }
     );
   }
-  
-  else {
-    return new Response("INTERNAL SERVER ERROR", { status: 500 });
-  }
+
 };  
