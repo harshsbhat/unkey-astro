1445a5eaff0377892e4051949a07572ec7923de9
Harsh Shrikant Bhat <90265455+harshsbhat@users.noreply.github.com>
2024-10-17 21:31:57 +0530

    first commit

---
+++ src/lib/unkey.ts
@@ -0,0 +1,9 @@
+import { Ratelimit } from "@unkey/ratelimit"
+
+export const unkey = new Ratelimit({
+  rootKey: import.meta.env.UNKEY_ROOT_KEY,
+  namespace: "astro",
+  limit: 3,
+  duration: "30s",
+  async: true
+})
---
+++ src/pages/api/ratelimited.ts
@@ -0,0 +1,18 @@
+import type { APIRoute } from 'astro';
+import { unkey } from "../../lib/unkey";
+
+export const GET: APIRoute = async ({ request }) => {
+  const ipAddress = request.headers.get("x-forwarded-for") ?? "anonymous"
+  const ratelimit = await unkey.limit(ipAddress);
+
+  if (!ratelimit.success) {
+    return new Response("Try again later", { status: 429 });
+  }
+
+  return new Response("Protected route accessed", {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  });
+};
